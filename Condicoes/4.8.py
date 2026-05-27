10b4813a80cad5d128da63b4ddf867b15102e64a
Felipe Mota <felipemota465@gmail.com>
2026-05-27 17:04:16 -0300

    Condicoes

--- Condicoes/4.8.py
+++ Condicoes/4.8.py
@@ -1,6 +1,6 @@
 num1 = float(input("Digite um numero: "))
 num2 = float(input("Digite outro numero: "))
-op = input("Escolha uma operacao: (+, -, *, /)")
+op = input("Escolha uma operacao: (+, -, *, /) ")
 
 if op == "+":
     res = num1 + num2
@@ -20,6 +20,6 @@ elif op == "/":
         print(f"Resultado: {res}")
     else:
         print(f"Erro: divisao por zero!")
-        
+
 else:
     print(f"Operacao invalida!")
