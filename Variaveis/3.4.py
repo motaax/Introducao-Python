f589ba9b1ceb0ed20db48861a3036d8651c6a58e
Felipe Mota <felipemota465@gmail.com>
2026-05-27 16:18:54 -0300

    Variaveis

---
+++ Variaveis/3.4.py
@@ -0,0 +1,3 @@
+salario = float(input("Digite o seu salario: R$ "))
+paga_imposto = salario > 1200
+print(paga_imposto)
---
+++ Variaveis/3.6.py
@@ -0,0 +1,6 @@
+materia1 = float(input("Digite a nota da matéria 1: "))
+materia2 = float(input("Digite a nota da matéria 2: "))
+materia3 = float(input("Digite a nota da matéria 3: "))
+
+aprovado = materia1 > 7 and materia2 > 7 and materia3 > 7
+print(aprovado)
