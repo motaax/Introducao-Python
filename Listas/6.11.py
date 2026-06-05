50b887d826eebb8396cef0dcff014001b83a6e61
Felipe Mota <felipemota465@gmail.com>
2026-06-05 19:01:15 -0300

    Listas

---
+++ Listas/6.11.py
@@ -0,0 +1,8 @@
+L = [1,7,2,4,30]
+
+max = L[0] 
+for e in L:
+    if e > max:
+        max = e
+
+print(max)
---
+++ Listas/6.12.py
@@ -0,0 +1,8 @@
+L = [3,7,2,4,1,30]
+
+min = L[0] 
+for e in L:
+    if e < min:
+        min = e
+
+print(min)
