399e09c1032201670d86656ff454986207ef4a80
Felipe Mota <felipemota465@gmail.com>
2026-06-07 19:20:09 -0300

    Funcoes

---
+++ Funções/8.14.py
@@ -0,0 +1,9 @@
+import random # import random as r ; from random import randint
+
+n = random.randint(1, 10)
+x = int(input("Escolha um numero entre 1 e 10: "))
+
+if x == n:
+    print("Voce acertou!")
+else:
+    print("Voce errou!")
