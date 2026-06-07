fcf51b3aeaacfd01455895f8b41387fe2f691eb5
Felipe Mota <felipemota465@gmail.com>
2026-06-07 18:33:45 -0300

    Funcoes

---
+++ Funções/8.7.py
@@ -0,0 +1,7 @@
+def area(b, h):
+    return (b * h) / 2
+
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+
+print(f"A area do triangulo e: {area(num1, num2)} m²")
